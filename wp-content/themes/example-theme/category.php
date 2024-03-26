320e0fcccbe6afc5cd03dc215cb03f50977df2f8
VeetiSo <v.sorakivi@gmail.com>
2024-03-26 15:48:26 +0200

    add

--- wp-content/themes/example-theme/category.php
+++ wp-content/themes/example-theme/category.php
@@ -1,4 +1,5 @@
 <?php
+global $wp_query;
 get_header();
 ?>
 
@@ -6,32 +7,22 @@ get_header();
     <section class="hero">
         <div class="hero-text">
             <?php
-            if ( have_posts() ) :
-                while ( have_posts() ) :
-                    the_post();
-                    the_title('<h1>', '</h1>');
-                    the_content();
-                endwhile;
-            else :
-                _e( 'Sorry, no posts matched your criteria.', 'esimerkki' );
-            endif;
+                echo '<h1>' . single_cat_title('', false) . '</h1>';
+                echo '<p>' . category_description() . '</p>';
             ?>
         </div>
 
-        <?php the_custom_header_markup(); ?>
-        <!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/map.svg" alt="hero"> -->
+
+        <img src="<?php echo get_random_post_image(get_queried_object_id()); ?>" alt="hero">
     </section>
     <main>
         <section class="products">
-            <h2>Featured Products</h2>
+            <h2><?php single_cat_title(); ?></h2>
             <?php
-            $args = ['tag' => 'featured', 'posts_per_page' => 3];
-            $products = new WP_Query($args);
-            generate_article($products);
+            generate_article($wp_query);
             ?>
         </section>
     </main>
 
 <?php
-get_sidebar();
 get_footer();
